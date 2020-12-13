cfbbd9b83c4de7ee55d5a3aa87a50061d5a89e85
Nick Allen <nwestallen@gmail.com>
2020-12-13 10:21:46 -0500

    Completed task 4

--- index.js
+++ index.js
@@ -102,8 +102,14 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningFunct) {
+  let score = {
+    "Home": 0,
+    "Away": 0
+  }
+  score["Home"] += this.inningFunct;
+  score["Away"] += this.inningFunct;
+  return score;
 }
 
 
